fcaf8687a43e08fa515dec24e7a9279072fddb09
David Johnson <davidjohnson@MacBook-Pro-46.local>
2020-05-23 08:40:42 +0100

    Added basic layout components for body and footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,16 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { SocialMediaBarComponent } from './social-media-bar/social-media-bar.component';
+import { BodyComponent } from './body/body.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
-  declarations: [AppComponent, SocialMediaBarComponent],
+  declarations: [
+    AppComponent,
+    SocialMediaBarComponent,
+    BodyComponent,
+    FooterComponent,
+  ],
   imports: [BrowserModule, FontAwesomeModule],
   providers: [],
   bootstrap: [AppComponent],
--- src/app/social-media-bar/social-media-bar.component.ts
+++ src/app/social-media-bar/social-media-bar.component.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import { faFacebookSquare } from '@fortawesome/free-brands-svg-icons';
 
 @Component({
   selector: 'social-media-bar',
   templateUrl: './social-media-bar.component.html',
   styleUrls: ['./social-media-bar.component.less'],
 })
-export class SocialMediaBarComponent {}
+export class SocialMediaBarComponent {
+  faFacebookSquare = faFacebookSquare;
+}
